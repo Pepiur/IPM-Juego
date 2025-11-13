80a962cbdd6150aa6248973848c54310b4c6e6aa
Pepiur <josepepes2011@gmail.com>
2025-11-13 19:48:40 +0100

    BASE ESCENAS

---
+++ PomoFarm/Assets/Scripts/GestorEscenas.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GestorScenas : MonoBehaviour
+{
+
+    public void CargarIncio()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void CargarJuego()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void CargarAjustes()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CargarPlanificar()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void CargarCalendario()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void CargarTiempoEstudio()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void CargarPersonalizar()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
